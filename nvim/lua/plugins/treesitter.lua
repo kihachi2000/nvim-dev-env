ea8b5502c1f99fd574028908f403f5087321768e
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2024-04-24 19:02:52 +0900

    feat: add typescript parser

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "typescript",
+            },
             highlight = {
                 enable = true,
                 disable = { "vim" },
