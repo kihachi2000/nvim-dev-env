6bc93476b4b5ca8fd1015242a0929a2a753698e4
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2024-07-10 16:08:48 +0900

    feat: add volume of honori directory

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,4 +1,5 @@
 -- yash読み込み
 vim.opt.runtimepath:append("$HOME/yash")
+vim.opt.runtimepath:append("$HOME/honori")
 
 vim.cmd([[colorscheme yash]])
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 VOLUMES=(
     "-v" "$HOME/git/yash:/root/yash"
+    "-v" "$HOME/git/honori:/root/honori"
 )
 
 docker run -it --rm "${VOLUMES[@]}" nvim-dev-env
